c8ea23986d863ce6fb55e5a49cfea4e3fb8763d1
zuparomana <adam.romanowski93@gmail.com>
2018-09-17 14:06:28 +0200

    Add handling with annotations

--- src/main/java/webserver/App.java
+++ src/main/java/webserver/App.java
@@ -1,13 +1,45 @@
 package webserver;
 
+import annotations.WebRoute;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.net.InetSocketAddress;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        String[] controllers = {"IndexController", "PageController"};
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        for (String controller : controllers) {
+            Class obj = Class.forName("webserver." + controller);
+
+            Constructor constructor = obj.getConstructor();
+            Method[] methods = obj.getMethods();
+
+            for(Method method : methods) {
+                Annotation annotation = null;
+                if (method.getName().equals("handle")) {
+                    annotation = method.getAnnotation(WebRoute.class);
+                }
+                if (annotation instanceof WebRoute) {
+                    WebRoute webRoute = (WebRoute) annotation;
+                    server.createContext(webRoute.path(), (HttpHandler) constructor.newInstance());
+                }
+            }
+        }
+
+        server.setExecutor(null);
+        server.start();
     }
 }
